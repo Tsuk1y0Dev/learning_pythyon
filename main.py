ba6d561c430a8965a616e68b403921399d77d4f1
Tsuk1y0Dev <tsuk1y0develope@gmail.com>
2025-09-10 18:43:18 +0900

    12 заданий

--- main.py
+++ main.py
@@ -1,8 +1,74 @@
-a = int(input())
-if a != 0:
-    if a % 2 == 0:
-        print('Чётное')
-    else: 
-        print('Нечётное')
-else:
-    print("Нейтральное")
+# a = int(input())
+# if a != 0:
+#     if a % 2 == 0:
+#         print('Чётное')
+#     else: 
+#         print('Нечётное')
+# else:
+#     print("Нейтральное")
+
+# Второе
+# a = input()
+# print(a[-1])
+
+#Третье
+# first_word = input()
+# second_word = input()
+# if first_word[0] == second_word[0]:
+#     print('Первые буквы совпадают')
+# else:
+#     print("Первые буквы разные")
+
+# Четвёртое
+# word = input()
+# if word[-1] == "ь":
+#     print(word[-2])
+# else:
+#     print(word[-1])
+
+#Пятое
+# integer = input()
+# print(int(integer[0]) + int(integer[-1]))
+
+#Шестое
+# first_integer = int(input())
+# second_integer = int(input())
+# print(first_integer[0] == second_integer[0])
+
+#Седьмое
+# first_integer = int(input())
+# second_integer = int(input())
+
+# print(first_integer % second_integer == 0)
+
+#Восьмое
+# a = int(input())
+# if a % 2 == 0:
+#     print('Кратное')
+# else: 
+#     print('Некратное')
+
+#Девятое
+# first_integer = int(input())
+# second_integer = int(input())
+# third_integer = int(input())
+
+# if (third_integer - second_integer) == (second_integer - first_integer):
+#     print('Из данных чисел можно составить арифметическую прогрессию')
+# else:
+#     print("нельзя")
+
+#Десятое
+# a = input()
+# if len(a) == 3:
+#     print(int(a[0])*int(a[1])*int(a[2]))
+
+#одинадцатое
+# a = input()
+# b = input()
+# c = input()
+# print((a == b) or (b==c) or (a==c))
+
+#Двенадцатое
+# integer = input()
+# print(integer[0] == integer[1])
